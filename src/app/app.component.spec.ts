c80e5508bb5f331f6bdb1f7bcca4f55efc1338d1
yvonnkr <yvonnkr86@gmail.com>
2024-10-06 19:39:02 +0100

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'whatsapp-clone-front' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('whatsapp-clone-front');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
