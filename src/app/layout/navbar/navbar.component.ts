3ff169c66b49bd218b4af64f7aa701bd8bc0211d
yvonnkr <yvonnkr86@gmail.com>
2024-10-09 19:21:00 +0100

    implement user search service, add new conversation component with toast service

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -2,7 +2,14 @@ import {Component, effect, inject} from '@angular/core';
 import {Oauth2AuthService} from "../../auth/oauth2-auth.service";
 import {ConnectedUser} from "../../shared/model/user.model";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import {NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle} from "@ng-bootstrap/ng-bootstrap";
+import {
+  NgbDropdown,
+  NgbDropdownItem,
+  NgbDropdownMenu,
+  NgbDropdownToggle,
+  NgbOffcanvas
+} from "@ng-bootstrap/ng-bootstrap";
+import {NewConversationComponent} from './new-conversation/new-conversation.component';
 
 @Component({
   selector: 'wac-navbar',
@@ -21,6 +28,7 @@ export class NavbarComponent {
 
   oauth2Service = inject(Oauth2AuthService);
   connectedUser: ConnectedUser | undefined;
+  offCanvasService = inject(NgbOffcanvas);
 
   constructor() {
     this.listenToFetchUser();
@@ -46,6 +54,7 @@ export class NavbarComponent {
   }
 
   openNewConversation() {
-
+    this.offCanvasService.open(NewConversationComponent,
+      {position: "start", container: "#main", panelClass: "offcanvas"})
   }
 }
